8c212721f03d9b63068934bad70782a2c8e3740c
Cassandra de Git <cassandra.m.degit@gmail.com>
2013-06-20 23:52:52 -0700

    Added separate page for checkout
    Submit button in cart takes you to checkout

--- shoppingcart.js
+++ shoppingcart.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {	// jQuery wrapper
 
-	// Checkout Code
+	// Shopping Cart Code
 	// sets a default price attribute for each item
-	$('.checkout-info').each( function(index) {
+	$('.cart-info').each( function(index) {
 		var price = $(this).children(".price").html();
 		price = price.slice(1);
 		price = parseFloat(price);
@@ -12,7 +12,7 @@ $(document).ready(function() {	// jQuery wrapper
 	// clicking on the quantity number inputs change the price attibute for an item and call updateTotal
 	$('.quantity').each( function(index) {
 		$(this).click( function() {
-			var checkout_obj = $(this).parents(".checkout-info");
+			var checkout_obj = $(this).parents(".cart-info");
 			var price = checkout_obj.children(".price").html();
 			price = price.slice(1);
 			price = parseFloat(price);
@@ -32,12 +32,12 @@ $(document).ready(function() {	// jQuery wrapper
 	// updates the total price
 	function updateTotal() {
 		var newTotal = 0;
-		$('.checkout-info').each( function(index) {
+		$('.cart-info').each( function(index) {
 			newTotal += parseFloat($(this).attr("price"));
 		});
 		
 		newTotal = newTotal.toFixed(2);
 		$("#total").html("Item total: $" + newTotal);
 	}
-
+	
 });
